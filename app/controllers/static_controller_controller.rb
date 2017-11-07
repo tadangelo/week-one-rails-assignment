28ecfb245197715d59407256cc3a25b019fb147c
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-07 00:44:14 -0600

    Completed week one forked repo project

---
+++ app/controllers/static_controller_controller.rb
@@ -0,0 +1,7 @@
+class StaticControllerController < ApplicationController
+  def home
+  end
+
+  def quotes
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :guides
+  
+  root to: 'static_controller#home'
+
+  get 'static_controller/quotes'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
